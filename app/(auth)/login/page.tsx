6a18fbcb4fd73cd462797b0b134fe88bd31b63ec
Steph Kanji <stephKanji@gmail.com>
2026-07-27 09:19:05 +0300

    err

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
-// app/(auth)/login/page.tsx
-import { Suspense } from 'react'
 import LoginForm from './login-form'
+import { Suspense } from 'react'
 
 export default function LoginPage() {
   return (
